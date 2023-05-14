fb9700ff4a73ce0a0e15542ab59da60c5b64f7c1
mstfkarakas <mstfkarakas@gmail.com>
2023-05-15 00:54:09 +1000

    ...

---
+++ src/test/java/day13_writeExcel_Screenshot/C01_ReadExcel.java
@@ -0,0 +1,22 @@
+package day13_writeExcel_Screenshot;
+
+import org.junit.Test;
+
+public class C01_ReadExcel {
+
+    @Test
+    public void test01() {
+
+        // excel dosyalari bilgisayarimizda oldugu icin
+        // Java'dan FileInputStream class'i yardimiyla ulasacagiz
+
+
+
+
+        // satir ve sutun bilgisini parametre olarak alip
+        // o hucredeki bilgiyi yazdiran bir method olusturun
+
+    }
+
+
+}
