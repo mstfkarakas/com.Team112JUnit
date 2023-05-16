d4baa8f349f1c477aa53511d6c8107a6f0c2df03
mstfkarakas <mstfkarakas@gmail.com>
2023-05-16 22:48:15 +1000

    ...

--- src/test/java/day13_writeExcel_Screenshot/C01_ReadExcel.java
+++ src/test/java/day13_writeExcel_Screenshot/C01_ReadExcel.java
@@ -1,22 +1,44 @@
 package day13_writeExcel_Screenshot;
 
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.junit.Test;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class C01_ReadExcel {
 
     @Test
-    public void test01() {
+    public void test01() throws IOException {
 
         // excel dosyalari bilgisayarimizda oldugu icin
         // Java'dan FileInputStream class'i yardimiyla ulasacagiz
 
 
+        String filePath = "src/test/java/day12_webtables_excel/countries.xlsx";
+        FileInputStream fis = new FileInputStream(filePath);
+
+        Workbook workbook = WorkbookFactory.create(fis);
+
+        System.out.println(workbook.getSheet("Sayfa1").getRow(3).getCell(1));
 
 
         // satir ve sutun bilgisini parametre olarak alip
         // o hucredeki bilgiyi yazdiran bir method olusturun
 
+        printData(25,2);
+        printData(14,3);
     }
 
+    private void printData(int row, int coloumn) throws IOException {
 
+        String filePath = "src/test/java/day12_webtables_excel/countries.xlsx";
+        FileInputStream fis = new FileInputStream(filePath);
+
+        Workbook workbook = WorkbookFactory.create(fis);
+
+        System.out.println(workbook.getSheet("Sayfa1").getRow(row).getCell(coloumn));
+    }
 }
---
+++ src/test/java/day13_writeExcel_Screenshot/C02_WriteExcel.java
@@ -0,0 +1,13 @@
+package day13_writeExcel_Screenshot;
+
+import org.junit.Test;
+
+public class C02_WriteExcel {
+
+    @Test
+    public void test01(){
+
+
+
+    }
+}
